97ca66da88c9e3166a5651252adfb6e536fe861a
Nctine <ponlakritkm@gmail.com>
2020-03-31 22:46:36 +0700

    add layout

---
+++ app/src/main/java/com/example/demochatapp/ChatRoomActivity.kt
@@ -0,0 +1,12 @@
+package com.example.demochatapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatRoomActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_room)
+    }
+}

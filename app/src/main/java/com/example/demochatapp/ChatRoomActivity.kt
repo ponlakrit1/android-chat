ca4f07e2cd16655802e694d980c6f9b895a35ab9
Nctine <ponlakritkm@gmail.com>
2020-04-04 16:14:14 +0700

    add chat room page

--- app/src/main/java/com/example/demochatapp/ChatRoomActivity.kt
+++ app/src/main/java/com/example/demochatapp/ChatRoomActivity.kt
@@ -1,12 +1,49 @@
 package com.example.demochatapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.chat_room_dialog.view.*
+import java.util.*
 
 class ChatRoomActivity : AppCompatActivity() {
+
+    lateinit var lvMessage : ListView;
+
+    private var chatArray: ArrayList<String>? = null
+    private var chatAdapter: ArrayAdapter<String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_room)
+
+        chatArray = ArrayList<String>();
+
+        chatAdapter = ArrayAdapter<String>(applicationContext, android.R.layout.simple_list_item_1, chatArray);
+        lvMessage = findViewById<ListView>(R.id.lvMessage);
+        lvMessage.setAdapter(chatAdapter)
+
+        val mFab = findViewById<FloatingActionButton>(R.id.fabAddChat)
+        mFab.setOnClickListener {
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.chat_room_dialog, null)
+            val mBuilder = AlertDialog.Builder(this).setView(mDialogView).setTitle("CHAT TO")
+            val mAlertDialog = mBuilder.show()
+
+            mDialogView.dialogChatBtn.setOnClickListener {
+                mAlertDialog.dismiss()
+
+                val name = mDialogView.chatTo.text.toString()
+                Toast.makeText(applicationContext, name, Toast.LENGTH_LONG).show();
+            }
+            mDialogView.dialogCloseBtn.setOnClickListener {
+                mAlertDialog.dismiss()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/demochatapp/MainActivity.kt
+++ app/src/main/java/com/example/demochatapp/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.demochatapp
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
@@ -8,12 +11,8 @@ import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
-import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
-import com.google.gson.GsonBuilder
 import org.json.JSONArray
 import org.json.JSONObject
-import java.lang.String.format
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +20,9 @@ class MainActivity : AppCompatActivity() {
     var ROOT_URL = "http://192.168.39.148:3000";
 
     private var username: EditText? = null;
+
+    lateinit var prefs : SharedPreferences;
+    lateinit var editor : SharedPreferences.Editor;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +38,21 @@ class MainActivity : AppCompatActivity() {
         if (txtUsername.toString() !== "") {
             val jsonObjectRequest = JsonArrayRequest(Request.Method.GET, ROOT_URL + "/user/"+txtUsername, null,
                 Response.Listener<JSONArray> { response ->
-                    Toast.makeText(applicationContext, response.toString(), Toast.LENGTH_SHORT).show();
+
+                    prefs = getSharedPreferences("UserPref", Context.MODE_PRIVATE);
+
+                    if(response.length() != 0){
+                        var obj : JSONObject = response.getJSONObject(0);
+
+                        editor = prefs.edit();
+                        editor.putString("username", obj.getString("USERNAME"));
+                        editor.apply();
+                    }
+
+//                    Toast.makeText(applicationContext, prefs.getString("username", ""), Toast.LENGTH_SHORT).show();
+
+                    val intent = Intent(this, ChatRoomActivity::class.java);
+                    startActivity(intent);
                 },
                 Response.ErrorListener { error ->
                     Toast.makeText(applicationContext, "$error", Toast.LENGTH_LONG).show();
